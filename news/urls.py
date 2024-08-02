43d2758e00097e000d3916e289cabb7038462d16
kunai <neni23@mail.ru>
2024-08-02 22:32:45 +0300

    added urls and views

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from news.views import NewsListView, NewsDetailView
+from news.views import NewsListView, NewsDetailView, NewsCreateView, NewsUpdateView
 
 urlpatterns = [
     path('', NewsListView.as_view(), name='news_list'),
-    path('<int:pk>', NewsDetailView.as_view(), name='news_detail')
+    path('<int:pk>', NewsDetailView.as_view(), name='news_detail'),
+    path('create/', NewsCreateView.as_view(), name='news_create'),
+    path('<int:pk>/update/', NewsUpdateView.as_view(), name='news_update'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.views import View, generic
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-from news.models import News
-from news.forms import NewsForm
+from .models import News, Comments
 
 
 class NewsListView(View):
@@ -14,5 +15,50 @@ class NewsListView(View):
                       {'news_list': news_list})
 
 
-class NewsDetailView(generic.DetailView):
-    model = News
+class NewsDetailView(View):
+
+    def get(self, request, pk):
+        news = News.objects.get(pk=pk)
+        comments = Comments.objects.filter(news=news)
+        return render(request,
+                      'news/news_detail.html',
+                      {'news': news, 'comments': comments})
+
+    def post(self, request, pk):
+        news = News.objects.get(pk=pk)
+        author = request.POST['user_name']
+        comment = request.POST['comment']
+        Comments.objects.create(news=news, user_name=author, comment=comment)
+        return redirect('news_detail', pk=pk)
+
+
+class NewsCreateView(LoginRequiredMixin, View):
+
+    def get(self, request):
+        return render(request,
+                      'news/news_create.html')
+
+    def post(self, request):
+        title = request.POST['title']
+        content = request.POST['content']
+        new_news = News.objects.create(title=title, content=content)
+        return redirect('news_list')
+
+
+class NewsUpdateView(LoginRequiredMixin, View):
+
+    def get(self, request, pk):
+        news = News.objects.get(pk=pk)
+        return render(request,
+                      'news/news_update.html',
+                      {'news': news})
+
+    def post(self, request, pk):
+        title = request.POST['title']
+        content = request.POST['content']
+        news = News.objects.get(pk=pk)
+        news.title = title
+        news.content = content
+        news.edit_date = timezone.now()
+        news.save()
+        return redirect('news_list')
