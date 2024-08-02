9f7906c4cde2899d5aebef31f053c7ff2ad493d9
kunai <neni23@mail.ru>
2024-08-02 20:19:27 +0300

    first page, all news

--- news/models.py
+++ news/models.py
@@ -1,3 +1,27 @@
+from datetime import timezone
+
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=25, verbose_name='Название')
+    content = models.TextField(verbose_name='Содержание')
+    pub_date = models.DateTimeField(verbose_name='Дата публикации', auto_now_add=True)
+    edit_date = models.DateTimeField(verbose_name='Дата редактирования', auto_now=True, null=True)
+    archive_status = models.BooleanField(verbose_name='В архиве', default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['pub_date']
+
+
+class Comments(models.Model):
+    user_name = models.CharField(max_length=25, verbose_name='Имя пользователя')
+    comment = models.TextField(verbose_name='Комментарий')
+    pub_date = models.DateTimeField(verbose_name='Дата комментария', auto_now_add=True)
+    news = models.ForeignKey('News', on_delete=models.CASCADE, verbose_name='Новость')
+
+    def __str__(self):
+        return self.comment
--- news/views.py
+++ news/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View, generic
 
-# Create your views here.
+from news.models import News
+from news.forms import NewsForm
+
+
+class NewsListView(View):
+
+    def get(self, request):
+        news_list = News.objects.all()
+        return render(request,
+                      'news/news_list.html',
+                      {'news_list': news_list})
+
+
+class NewsDetailView(generic.DetailView):
+    model = News
