a680ff182e7fb17b9e5e462770c2503fb0b25334
kunai <neni23@mail.ru>
2024-08-03 14:16:28 +0300

    added search

--- news/admin.py
+++ news/admin.py
@@ -4,13 +4,13 @@ from news.models import News, Comments
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'pub_date']
-
-
-admin.site.register(News, NewsAdmin)
+    search_fields = ['title']
 
 
 class CommentsAdmin(admin.ModelAdmin):
     list_display = ['id', 'user_name', 'pub_date']
+    search_fields = ['user_name']
 
 
+admin.site.register(News, NewsAdmin)
 admin.site.register(Comments, CommentsAdmin)
