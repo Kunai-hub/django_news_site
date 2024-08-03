867a55c144d5655118a8d192565b5de5144f19e0
kunai <neni23@mail.ru>
2024-08-03 16:00:28 +0300

    added group fields

--- news/admin.py
+++ news/admin.py
@@ -2,14 +2,36 @@ from django.contrib import admin
 from news.models import News, Comments
 
 
+class CommentsInline(admin.TabularInline):
+    model = Comments
+
+
 class NewsAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'pub_date']
     search_fields = ['title']
+    inlines = [CommentsInline]
+    fieldsets = (
+        ('Название и содержание', {
+            'fields': ('title', 'content')
+        }),
+        ('Архив', {
+            'fields': ('archive_status',)
+        })
+    )
 
 
 class CommentsAdmin(admin.ModelAdmin):
     list_display = ['id', 'user_name', 'pub_date']
     search_fields = ['user_name']
+    list_filter = ['user_name']
+    fieldsets = (
+        ('Новость', {
+            'fields': ('news',)
+        }),
+        ('Данные о комментарии', {
+            'fields': ('user_name', 'comment')
+        })
+    )
 
 
 admin.site.register(News, NewsAdmin)
